36206438a3a23d58f454dc030f9ffa157c94fd8b
Nina Adora <nirinakaes@gmail.com>
2026-07-25 14:57:45 +0300

    ensuring all dependencies have been installed

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from flask_cors import CORS
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -11,12 +12,12 @@ from app.extensions import db, migrate, jwt, bcrypt, cors, mail
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app, supports_credentials=True, origins=["https://usiwaste-726s.vercel.app"])
 
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
     bcrypt.init_app(app)
-    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
     mail.init_app(app)
 
     # Your code here — register blueprints, models, error handlers
