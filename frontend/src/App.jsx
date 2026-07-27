0c5b7d9826f1effbf2fce4c1b4d65aa4d5105c8d
Sharon Moegi <sharamoegi@gmail.com>
2026-07-27 16:52:47 +0300

    plugin tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,5 @@
+import Home from './pages/Home'
 
+export default function App() {
+  return <Home />
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import ListingCard from "../components/ListingCard";
 
+export default function Home (){
+    return (
+        <div className="min-h-screen bg-[#D9D9D9] p-8">
+            <ListingCard />
+        </div>
+    )
+}
