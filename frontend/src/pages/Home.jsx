cf52d495247784ae9147735cd217e2726b56c452
Sharon Moegi <sharamoegi@gmail.com>
2026-07-28 10:09:09 +0300

    Add function to handle Adding Items to Cart

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,24 @@ import Cart from "../components/cart";
 import { ShoppingCart } from "lucide-react";
 
 export default function Home (){
+     const [cart, setCart] = useState([]);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+  // Function to handle adding items
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
     return (
         <div className="min-h-screen bg-[#D9D9D9] p-8">
             <Hero />
